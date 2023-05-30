b34b8933635e8258842ec73d35013086df8627b1
sankara.kandyana <sankarrao.kandyana@gmail.com>
2023-05-30 16:17:38 +0400

    catalogue

--- payment.sh
+++ payment.sh
@@ -2,7 +2,7 @@ source common.sh
 component=catalogue
 
 roboshop_app_password=$1
-if [ -z "$roboshop_app_password"]; then
+if [ -z "$roboshop_app_password" ]; then
   echo "roboshop_app_password Is Missing"
 fi
 python
--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,7 @@ source common.sh
 component=shipping
 
 mysql_root_password=$1
-if [ -z "$mysql_root_password"]; then
+if [ -z "$mysql_root_password" ]; then
   echo "MySql Root Password Is Missing"
 fi
 maven
