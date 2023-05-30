09624f37d76eb24ddb6e31f3c1b429d97d7cb60f
sankara.kandyana <sankarrao.kandyana@gmail.com>
2023-05-30 17:37:50 +0400

    all

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,29 @@
-yum install nginx -y
+source common.sh
+component=frontend
+echo -e "${color} Installing Nginx Server ${nocolor} "
+yum install nginx -y &>>${log_file}
+stat_check $?
+
+
+echo -e "${color} Removing Old App Content ${nocolor} "
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+stat_check $?
+
+echo -e "${color} Downloading  $component  Content ${nocolor} "
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
+stat_check $?
+
+echo -e "${color} Extracting $component  Content ${nocolor} "
+cd /usr/share/nginx/html &>>${log_file}
+unzip /tmp/$component.zip &>>${log_file}
+stat_check $?
+
+echo -e "${color} Copy  $component  Conf File ${nocolor}"
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+stat_check $?
+
+echo -e "${color} Starting Nginx Server ${nocolor}"
 systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+stat_check $?
 systemctl restart nginx
+stat_check $?
